d69fdbd6293cd67937218eb20ebc84a0607377d6
hwang-yeongil <jyhwangy4k@gmail.com>
2022-07-18 16:25:43 +0900

    login use model in  header, make sign.js, modify css

---
+++ js/sign.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  //$('#username').focus();
+
+  $('#submit').click(function() {
+
+    event.preventDefault(); // prevent PageReLoad
+
+    var ValidEmail = $('#username').val() === '1234'; // User validate
+    var ValidPassword = $('#password').val() === '1234'; // Password validate
+
+      if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+          // $('.valid').css('display', 'block');
+          window.location = "https://github.com/hwang-yeongil/steamAG"; // go to home.html
+      }
+      // else {
+      //     $('.error').css('display', 'block'); // show error msg
+      // }
+  });
+});
